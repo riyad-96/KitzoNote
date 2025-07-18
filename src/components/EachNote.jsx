d2dc3196fac4dd3074f104c492daa62c882ee647
Riyad Hasan <riyadofficial.ide@gmail.com>
2025-07-18 22:06:01 +0600

    note adding through modal added

--- src/components/EachNote.jsx
+++ src/components/EachNote.jsx
@@ -1,11 +1,9 @@
 import { motion } from 'motion/react';
 import { formatDistanceToNow } from 'date-fns';
-import { MoreHorizontalSvg } from './Svgs';
-import { useEffect, useRef } from 'react';
 
 function EachNote({ note, func }) {
   const { id, title, text, createdAt } = note;
-  const { setContextMenu, setOnContextSelectedNoteId } = func;
+  const { openContextMenu } = func;
   const readableDate = createdAt?.toDate?.() || new Date();
   const relativeTime = formatDistanceToNow(readableDate, { addSuffix: true });
 
@@ -18,16 +16,16 @@ function EachNote({ note, func }) {
         opacity: 1,
         transition: { duration: 1 },
       }}
-      className="group relative rounded-lg border border-zinc-200 p-3 text-zinc-700 transition-colors hover:bg-zinc-100 hover:text-zinc-950"
+      className="space-y-3 group relative rounded-lg border border-zinc-200 p-3 text-zinc-700 transition-colors hover:bg-zinc-100 hover:text-zinc-950"
     >
-      <span className="text-xl font-medium">{title}</span>
-      <div className="line-clamp-4 min-h-[40px] whitespace-pre-wrap">{text}</div>
+      <span className="font-medium leading-tight line-clamp-1 text-lg">{title}</span>
+      <div className="line-clamp-4 min-h-[40px] whitespace-pre-wrap leading-snug">{text}</div>
 
       <span
         onContextMenu={(e) => {
           e.preventDefault();
-          setOnContextSelectedNoteId(id);
-          setContextMenu({ clientX: e.clientX, clientY: e.clientY, id: id, e });
+          const minRight = window.innerWidth - e.clientX;
+          openContextMenu({ clientX: minRight < 145 ? e.clientX - 128 : e.clientX, clientY: e.clientY, id });
         }}
         className="absolute inset-0 z-1 cursor-pointer"
       ></span>
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -2,9 +2,10 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import { useNotes, useUser } from '../contexts/contexts';
 
 import { db } from '../config/firebase';
-import { addDoc, collection, getDocs, orderBy, query, serverTimestamp } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, orderBy, query, serverTimestamp } from 'firebase/firestore';
 import { LoaderSvg } from './Svgs';
 import EachNote from './EachNote';
+import { AnimatePresence, motion } from 'motion/react';
 
 function Notes() {
   const { user } = useUser();
@@ -41,13 +42,56 @@ function Notes() {
     }
   }, [user.uid, fetchUserNotes]);
 
+  //! Modal and custom context menu
+  const [noteModalOpen, setNoteModalOpen] = useState(false);
+  const [contextMenu, setContextMenu] = useState({
+    visible: false,
+    x: 0,
+    y: 0,
+    id: '',
+  });
+  const contextRef = useRef(null);
+
+  function openContextMenu({ clientX, clientY, id }) {
+    console.log(clientX, clientY, id);
+    setContextMenu({
+      visible: true,
+      x: clientX,
+      y: clientY,
+      id,
+    });
+  }
+
+  useEffect(() => {
+    function handleOutsideClick() {
+      if (contextMenu.visible) {
+        setContextMenu((prev) => ({ ...prev, visible: false }));
+      }
+    }
+
+    document.addEventListener('click', handleOutsideClick);
+    return () => document.removeEventListener('click', handleOutsideClick);
+  }, [contextMenu.visible]);
+
   //! add notes
-  async function handleAddNote() {
+  const quickNoteTitleRef = useRef(null);
+  const quickNoteBodyRef = useRef(null);
+
+  useEffect(() => {
+    if (noteModalOpen && quickNoteTitleRef.current) {
+      quickNoteTitleRef.current.focus();
+    }
+  }, [noteModalOpen]);
+
+  const [quickNoteTitle, setQuickNoteTitle] = useState('');
+  const [quickNoteBody, setQuickNoteBody] = useState('');
+
+  async function handleAddNoteModal() {
     const notesCollectionRef = collection(db, 'users', user.uid, 'notes');
 
     const databaseNote = {
-      title: 'title',
-      text: 'amar notun note',
+      title: quickNoteTitle.trim(),
+      text: quickNoteBody,
       createdAt: serverTimestamp(),
     };
 
@@ -57,55 +101,46 @@ function Notes() {
 
       const localNote = {
         id: newNoteId,
-        title: 'title',
-        text: 'amar notun note',
+        title: quickNoteTitle.trim(),
+        text: quickNoteBody,
       };
       setNotes((prev) => [localNote, ...prev]);
+      setQuickNoteTitle('');
+      setQuickNoteBody('');
+      setNoteModalOpen(false);
     } catch (error) {
       console.error(error);
     }
   }
 
-  const [noteModalOpen, setNoteModalOpen] = useState(false);
-  const [isOptionContextOpen, setIsOptionContextOpen] = useState(false);
-  const [onContextSelectedNoteId, setOnContextSelectedNoteId] = useState('');
-  const contextRef = useRef(null);
+  //! Add to trash
+  async function handleTrash() {
+    const noteRef = doc(db, 'users', user.uid, 'notes', contextMenu.id);
 
-  function setContextMenu({ clientX, clientY, id, e }) {
-    const menu = contextRef.current;
-    menu.style.display = 'unset';
-    const minRight = window.innerWidth - e.clientX;
-    const { width } = menu.getBoundingClientRect();
-
-    if (minRight < 140) {
-      menu.style.left = `${clientX - width}px`;
-      menu.style.top = `${clientY}px`;
-    } else {
-      menu.style.left = `${clientX}px`;
-      menu.style.top = `${clientY}px`;
+    try {
+      const selectedNote = await getDoc(noteRef);
+      const localObj = {
+        ...selectedNote.data(),
+      };
+      console.log({ id: selectedNote.id, ...localObj });
+    } catch (error) {
+      console.error(error);
     }
   }
-
-  useEffect(() => {
-    const menu = contextRef.current;
-    function removeContext(e) {
-      if (!menu.contains(e.target)) {
-        menu.style.display = 'none';
-      }
-    }
-    document.addEventListener('click', removeContext);
-    return () => document.removeEventListener('click', removeContext);
-  }, []);
 
   return (
     <div className="">
       <div className="flex h-[60px] items-center justify-between">
         <h1 className="text-3xl font-medium">Notes</h1>
         <div className="flex items-center justify-end gap-2">
-          <button onClick={handleAddNote} className="h-[30px] cursor-pointer rounded-md border-1 border-zinc-300 bg-zinc-200 px-3 text-sm transition-colors hover:bg-zinc-300 active:translate-y-[1px]">
-            New Note
-          </button>
-          <button onClick={() => setNoteModalOpen(true)} className="h-[30px] cursor-pointer rounded-md border-1 border-zinc-300 bg-zinc-200 px-3 text-sm transition-colors hover:bg-zinc-300 active:translate-y-[1px]">
+          <button className="h-[30px] cursor-pointer rounded-md border-1 border-zinc-300 bg-zinc-200 px-3 text-sm transition-colors hover:bg-zinc-300 active:translate-y-[1px]">New Note</button>
+          <button
+            onClick={() => {
+              setNoteModalOpen(true);
+              console.log(quickNoteTitleRef.current);
+            }}
+            className="h-[30px] cursor-pointer rounded-md border-1 border-zinc-300 bg-zinc-200 px-3 text-sm transition-colors hover:bg-zinc-300 active:translate-y-[1px]"
+          >
             Quick Note
           </button>
         </div>
@@ -119,22 +154,84 @@ function Notes() {
         ) : (
           <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-2">
             {notes.map((note) => (
-              <EachNote key={note.id} note={note} func={{ setContextMenu, setOnContextSelectedNoteId }} />
+              <EachNote key={note.id} note={note} func={{ openContextMenu }} />
             ))}
           </div>
         )}
       </div>
 
-      <div ref={contextRef} onContextMenu={(e) => e.preventDefault()} className="fixed top-0 left-0 z-10 hidden rounded-lg bg-white p-1 shadow-md">
-        <div className="grid overflow-hidden rounded-md">
-          <button onClick={() => console.log(onContextSelectedNoteId)} className="flex cursor-pointer bg-zinc-50 px-3 py-2 text-sm transition-colors hover:border-zinc-200 hover:bg-zinc-200">
-            Edit
-          </button>
-          <button onClick={() => setNotes((prev) => prev.filter((note) => note.id !== onContextSelectedNoteId))} className="flex cursor-pointer bg-zinc-50 px-3 py-2 text-sm transition-colors hover:border-zinc-200 hover:bg-zinc-200">
-            Move to trash
-          </button>
+      {contextMenu.visible && (
+        <div onContextMenu={(e) => e.preventDefault()} onClick={(e) => e.stopPropagation()} style={{ top: contextMenu.y, left: contextMenu.x }} className="fixed top-0 left-0 z-10 rounded-lg bg-white p-1 shadow-md">
+          <div className="grid overflow-hidden rounded-md whitespace-nowrap">
+            <button onClick={() => console.log(contextMenu.id)} className="flex cursor-pointer bg-zinc-50 px-3 py-2 text-sm transition-colors hover:border-zinc-200 hover:bg-zinc-200">
+              Edit Note
+            </button>
+            <button onClick={handleTrash} className="flex cursor-pointer bg-zinc-50 px-3 py-2 text-sm transition-colors hover:border-zinc-200 hover:bg-zinc-200">
+              Move to trash
+            </button>
+            <button className="flex cursor-pointer bg-zinc-50 px-3 py-2 text-sm transition-colors hover:border-zinc-200 hover:bg-zinc-200">Delete note</button>
+          </div>
         </div>
-      </div>
+      )}
+
+      <AnimatePresence>
+        {noteModalOpen && (
+          <motion.div
+            onMouseDown={() => setNoteModalOpen(false)}
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+              transition: { duration: 0.2 },
+            }}
+            exit={{
+              opacity: 0,
+            }}
+            className="fixed inset-0 z-10 flex items-center justify-center bg-black/30 p-4"
+          >
+            <div onMouseDown={(e) => e.stopPropagation()} className="w-full max-w-[700px] space-y-4 rounded-xl bg-zinc-50 p-5">
+              <div className="grid rounded-lg border border-zinc-200 transition-colors focus-within:border-zinc-300">
+                <input
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      e.preventDefault();
+                      quickNoteBodyRef.current.focus();
+                    }
+                  }}
+                  ref={quickNoteTitleRef}
+                  value={quickNoteTitle}
+                  onChange={(e) => setQuickNoteTitle(e.target.value)}
+                  type="text"
+                  placeholder="Title"
+                  className="px-4 py-2 text-lg font-medium outline-none"
+                />
+                <textarea
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter' && e.shiftKey) {
+                      e.preventDefault();
+                      handleAddNoteModal();
+                    }
+                  }}
+                  ref={quickNoteBodyRef}
+                  value={quickNoteBody}
+                  onChange={(e) => setQuickNoteBody(e.target.value)}
+                  placeholder="Take a note..."
+                  className="max-h-[calc(100vh_-_300px)] min-h-[200px] px-4 py-2 outline-none"
+                ></textarea>
+              </div>
+              <div className="flex justify-end gap-2">
+                <button onClick={() => setNoteModalOpen(false)} className="cursor-pointer rounded-md border-1 border-zinc-300 bg-zinc-200 px-4 py-1 text-sm transition-colors hover:bg-zinc-300">
+                  Cancel
+                </button>
+                <button onClick={handleAddNoteModal} className="cursor-pointer rounded-md border-1 border-zinc-300 bg-zinc-200 px-4 py-1 text-sm transition-colors hover:bg-zinc-300">
+                  Save
+                </button>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
