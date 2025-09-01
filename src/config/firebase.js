794ee5d610abc849def8720962aefa643e7d658a
riyad-96 <riyadofficial.ide@gmail.com>
2025-09-01 14:04:15 +0600

    environment variables implemented(testing purpose)

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,15 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiQVO_1Re0rSu3K0pG657FycGW8cpX5WM",
-  authDomain: "zero-note-b18ed.firebaseapp.com",
-  projectId: "zero-note-b18ed",
-  storageBucket: "zero-note-b18ed.firebasestorage.app",
-  messagingSenderId: "554413115906",
-  appId: "1:554413115906:web:e17a8f2618f95d0ebff51b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
